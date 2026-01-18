f42d32816b9600af22e6a347c1bc53733e57c802
Akrifis <kadzzuta@gmail.com>
2026-01-18 08:09:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Конфигурация
 const CONFIG = {
-    GITHUB_REPO: "Akrifis/Hdfjjfjf-re',
+    GITHUB_REPO: 'Akrifis/Hdfjjfjf-re',
     BRANCH: 'main',
     ANIME_LIST_FILE: 'anime_list.json',
     GITHUB_RAW_URL: 'https://raw.githubusercontent.com',
